c0e3a04d55553424bb4ceef5644576de2fd00815
tuxiantian <tuxiantian@163.com>
2025-06-05 20:47:12 +0800

    画图的时候出现一个问题，当文本框在画布的下方时，A文本框去连接B文本框，B文本框在A文本框的下方，连线部分会被画布遮住，但是当全屏模式画图的时候，之前遮住的连线又能显示出来

--- src/components/DecisionFlowTool.js
+++ src/components/DecisionFlowTool.js
@@ -1597,7 +1597,7 @@ const DecisionFlowTool = React.forwardRef(({
                                     width: '100%',
                                     height: '100%',
                                     pointerEvents: 'none',
-                                    zIndex: 10 // 确保连接线在节点上方
+                                    zIndex: 10,overflow: 'visible'
                                 }}
                             >
                                 <defs>
